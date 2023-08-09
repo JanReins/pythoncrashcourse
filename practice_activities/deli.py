d6e456bf7695b49c82d6e6692985a92082d3c2c8
JanReins <140710185+JanReins@users.noreply.github.com>
2023-08-10 05:44:50 +0800

    08.10.23

--- practice_activities/deli.py
+++ practice_activities/deli.py
@@ -1,10 +1,19 @@
-sandwich_orders = ["hamburger", "cheeseburger", "hotdog", "club sandwich", "BLT",
-                 "peanut butter and jelly", "grilled cheese", "chicken salad sandwich",
-                 "tuna salad sandwich", "egg salad sandwich"]
+sandwich_orders = ["hamburger", "cheeseburger","pastrami", "hotdog", "club sandwich", "BLT",
+                 "peanut butter and jelly", "grilled cheese","pastrami", "chicken salad sandwich",
+                 "tuna salad sandwich", "egg salad sandwich", "pastrami",]
 finished_sandwiches = []
+
+print("The Deli has run out of pastrami\n")
+
+for pastrami in sandwich_orders:
+    sandwich_orders.remove("pastrami")
 
 for sandwich in sandwich_orders:
     print(f"I made you a {sandwich} bb")
     sandwich_orders.remove(sandwich)
     finished_sandwiches.append(sandwich)
 
+print("\nThese are all the sandwich made: \n")
+
+for sandwich in finished_sandwiches:
+    print(sandwich)
