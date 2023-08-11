6d5e1a11170295c4f0e907702795f0843db57417
JanReins <140710185+JanReins@users.noreply.github.com>
2023-08-12 07:30:25 +0800

    Update tshirt.py

--- practice_activities/tshirt.py
+++ practice_activities/tshirt.py
@@ -1,7 +1,7 @@
 #accepts size and t-shirt size
 #print a sentence summarizing the size and message on print
 
-def main(size, print_message):
+def main(size = "Large", print_message = "I love Python"):
     #size = input("Enter desired T- shirt size: \n")
     #print_message = input("Write desired statement print: \n")
     print('Size: ', size, '/', 'print message: ', print_message)
@@ -13,5 +13,16 @@ main("large", "CPA in transit")
 
 #call for 2nd time using keyword argument
 
-main(print_message = "CPA in transit", size = 'Large')
+main(print_message = "CPA in transit", size = "Medium")
 
+# Modify the make_shirt() function so that shirts are large by default 
+# with a message that reads I love Python.
+
+main()
+
+#Make a large shirt and a medium shirt with the default message
+main(size = "Small")
+main(size = "Medium")
+
+#and a shirt of any size with a different message.
+main(size = "Small", print_message = "I am doing my best")
