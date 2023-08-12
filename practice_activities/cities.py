c6c6590e74f848e80ddc31a76974c401b062803e
JanReins <140710185+JanReins@users.noreply.github.com>
2023-08-12 18:22:18 +0800

    08.12.2023

--- practice_activities/cities.py
+++ practice_activities/cities.py
@@ -1,13 +1,9 @@
 def describe_city(city, country = "Spain"):
-    city = input("Name a City: \n (enter quit to exit)")
-    country = input("Now name the country in it located \n ")
-    print(f"The beautiful city of {city} is located in the {country}. ")
-    if city == "quit":
-            break
-    
+    print(f"The beautiful city of {city.title()} is located in the {country.titled()}. ")
 
-describe_city("\n")
+
+describe_city("Dubaii")
 describe_city("Portugal", "Europe")
-describe_ciry("Franch", "Europe")
+describe_city("Franch", "Europe")
 
 #improve this code like the chatGPT generated
---
+++ practice_activities/formatted_name.py
@@ -0,0 +1,5 @@
+def get_format_name(first_name, last_name):
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+musician = get_format_name("jimi", "hendrixson")
