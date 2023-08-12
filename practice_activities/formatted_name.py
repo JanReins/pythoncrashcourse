1406ad12961d55f2ef3f4bd9f7511ef6acbea98b
JanReins <140710185+JanReins@users.noreply.github.com>
2023-08-12 18:49:39 +0800

    08.12.2023

--- practice_activities/formatted_name.py
+++ practice_activities/formatted_name.py
@@ -1,5 +1,12 @@
-def get_format_name(first_name, last_name):
-    full_name = f"{first_name} {last_name}"
+def get_format_name(first_name, last_name, middle_name=""):
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
     return full_name.title()
 
 musician = get_format_name("jimi", "hendrixson")
+print(musician)
+
+musician = get_format_name('john', 'hooker', 'lee')
+print(musician)
