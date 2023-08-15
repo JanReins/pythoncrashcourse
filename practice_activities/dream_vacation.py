60a61aff60e05372c483684a29da6f195dd6bed1
JanReins <140710185+JanReins@users.noreply.github.com>
2023-08-16 05:01:40 +0800

    08.16.2023

--- practice_activities/dream_vacation.py
+++ practice_activities/dream_vacation.py
@@ -1,13 +1,2 @@
-dream_vacations = {}
-
-while True:
-    name = input("Hi can I ask your name? enter 'quit' or 'exit' to stop ")
-    if name == 'quit' or name  == 'exit':
-        break
-    dream_vacation = input(f"Hi {name}, if you could visit one place in the world, where would you go? ")
-    dream_vacations[name] = dream_vacation
-
-print("Dream vacation results ")
-for name, dream_vacation in dream_vacations.items():
-    print(f"{name} would like to visit {dream_vacation}")
-
+dream_vacation = input("What's the one place you'd like to visit the most, if you could go anywhere in the worl.")
+ 
