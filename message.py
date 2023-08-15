3d47585967255e7adc091d8e12d8a5610425c4d7
JanReins <140710185+JanReins@users.noreply.github.com>
2023-08-16 05:08:53 +0800

    08.16.2023

---
+++ message.py
@@ -0,0 +1,18 @@
+def show_message(text_messages):
+    for text in text_messages:
+        print(text)
+
+text_messages = [
+    "Hey, how's it going?",
+    "I'll be there in 5 mins.",
+    "Movie night this weekend?",
+    "Happy to help!",
+    "See you soon!",
+    "Running late, sorry!",
+    "Great news – promotion!",
+    "Need your opinion.",
+    "Check this out: 🤣",
+    "Can't make it, sorry.",
+]
+
+show_message(text_messages)
