860b98d81b25ac839d2accc8500e2bd5e2a0c923
JanReins <140710185+JanReins@users.noreply.github.com>
2023-08-18 07:44:06 +0800

    08.18.2023

--- classes/user.py
+++ classes/user.py
@@ -1,26 +1,53 @@
 class User:
     def __init__(self, first_name, last_name, age=0, gender=""):
-        self.firstname = first_name
-        self.lastname = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
         self.gender = gender
+        self.login_attempts = 0
 
     def describe_user(self):
-        print(self.firstname, self.lastname, self.age, self.gender)
+        print(self.first_name, self.last_name, self.age, self.gender)
 
         
     def greet_user(self):
-        print(f"Greetings {self.firstname}, son of {self.lastname}, {self.gender} and {self.age} years of age,\n")
+        print(f"Greetings {self.first_name}, son of {self.last_name}, {self.gender} and {self.age} years of age.")
+        print("Welcome to the accendants! \n")
+
+    def increment_login_attempts(self, attempts=1):
+        self.login_attempts += attempts
+        print(f"login attemp number {self.login_attempts}\n")
+
+    def reset_login_attempts(self, reset_login=1):
+        if self.login_attempts > 3:
+            self.login_attempts = 0
+            print("Login attempts reset.\n")
+        else:
+            print("No need to reset login attempts.\n")
+
 
 user1 = User('Jan', 'Rein', 24, 'Male')
 user2 = User('Airah', 'Meneses', 25, 'Female')
-user3 = User('Draig', 'Rein', 30)
 
 user1.describe_user()
 user1.greet_user()
+user1.increment_login_attempts()
 
 user2.describe_user()
 user2.greet_user()
+user2.increment_login_attempts()
 
-user3.describe_user()
-user3.greet_user()
+user1.increment_login_attempts()
+user2.increment_login_attempts()
+
+user1.increment_login_attempts()
+user2.increment_login_attempts()
+
+user1.increment_login_attempts()
+user2.increment_login_attempts()
+
+user1.reset_login_attempts()
+user2.reset_login_attempts()
+
+user1.increment_login_attempts()
+user2.increment_login_attempts()
