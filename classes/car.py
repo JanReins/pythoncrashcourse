eb7a52e677a40e7ebb24a6d55f7ff283bbebed31
JanReins <140710185+JanReins@users.noreply.github.com>
2023-08-18 06:58:22 +0800

    08.18.2023

--- classes/car.py
+++ classes/car.py
@@ -7,3 +7,6 @@ class Car:
     def get_descriptive_name(self):
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
