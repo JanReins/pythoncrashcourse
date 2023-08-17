44fd1ee1bfb9ce7893b85d2bd2a2a3c0768beca3
JanReins <140710185+JanReins@users.noreply.github.com>
2023-08-17 10:43:46 +0800

    08.17.2023

--- classes/restaurant.py
+++ classes/restaurant.py
@@ -5,12 +5,17 @@ class Restaurant:
     
 
     def describe_restaurant(self):
-        print(f"The {self.restaurant_name} is a {self.cuisin_type} 5 star restuarant")
+        print(f"The {self.restaurant_name} is a {self.cuisin_type} cuisin 5 star restuarant")
 
     def open_restaurant(self):
         print(f'{self.restaurant_name} is open from 1pm to 12 am')
 
     
-cebu_restaurant = Restaurant("Rico's Lechon", "Roasted suckling pig")
-cebu_restaurant.describe_restaurant()
-cebu_restaurant.open_restaurant()
+cebu_restaurant_1 = Restaurant("Rico's Lechon", "Roasted suckling pig")
+cebu_restaurant_2 = Restaurant("La Marea", "Seafood dishes")
+cebu_restaurant_3 = Restaurant("Lantaw Native Floating Restaurant", "Filipino dishes")
+
+cebu_restaurant_1.describe_restaurant()
+cebu_restaurant_2.describe_restaurant()
+cebu_restaurant_3.describe_restaurant()
+#cebu_restaurant.open_restaurant()
