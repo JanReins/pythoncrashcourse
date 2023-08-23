b7b23a47e5e91bbde6fa5e91df35c6c5a69ed3b0
JanReins <140710185+JanReins@users.noreply.github.com>
2023-08-23 17:09:40 +0800

    08.23.2023

--- greet_users.py
+++ greet_users.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 import json
 
-path = Path('username.json')
-if path.exists():
-    names = path.read_text()
-    username = json.loads(names)
-    print(f"Welcome back, {username}!")
-else:
-    username = input("What is your name? ")
-    names = json.dumps(username)
-    path.write_text(names)
-    print(f"We'll remember you when you come back, {username}!")
+def greet_user():
+    path = Path('username.json')
+    if path.exists():
+        names = path.read_text()
+        username = json.loads(names)
+        print(f"Welcome back, {username}!")
+    else:
+        username = input("What is your name? ")
+        names = json.dumps(username)
+        path.write_text(names)
+        print(f"We'll remember you when you come back, {username}!")
+
+    greet_user()
