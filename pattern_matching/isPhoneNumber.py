8d84cbf86d8a03fde62be3161c58e8d60e10ce70
JanReins <140710185+JanReins@users.noreply.github.com>
2023-08-25 09:04:37 +0800

    08.25.2023

--- pattern_matching/isPhoneNumber.py
+++ pattern_matching/isPhoneNumber.py
@@ -1,22 +1,9 @@
-def isPhoneNumber(text):
-    if len(text) != 12:
-        return False
-    for i in range(0,3):
-        if not text[i].isdecimal():
-            return False
-        if text[3] != '-':
-            return False
-        
-    for i in range(4,7):
-        if not text[i].isdecimal():
-            return False
-        if text[7] != '-':
-            return False
-        for i in range(8,12):
-            if not text[1].isdecimal():
-                return False
+import re
 
-print('Is 415-555-4242 a phone number?')
-print(isPhoneNumber('415-555-4242'))
-print('Is Moshi moshi a phone number?')
-print(isPhoneNumber('Moshi moshi'))
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My number is 415-555-4242.')
+print(mo.group(1))
+print(mo.group(2))
+print('Phone number found: ' + mo.group())
+print('Phone number found: ' + mo.group(0))
+
